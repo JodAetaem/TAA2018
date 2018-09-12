c11f2c2608e9d8a892ef8f515209a34c4809f119
JodAetaem <clarzi@gmail.com>
2018-09-12 17:44:47 +0200

    initial commit

---
+++ src/main/java/istic2018/taa/JpaTest.java
@@ -0,0 +1,33 @@
+package istic2018.taa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("dev");
+		EntityManager manager = factory.createEntityManager();
+
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		try {
+			
+			
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		tx.commit();
+		
+		manager.close();
+		factory.close();
+	}
+
+}
