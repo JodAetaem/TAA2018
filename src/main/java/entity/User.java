b62b0a447c646e79417fb3b7051f5ae4c3f07488
redeyes361 <redeyes_361@hotmail.com>
2018-09-14 09:58:43 +0200

    toto

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -4,11 +4,10 @@ import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.Id;	
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class User {
