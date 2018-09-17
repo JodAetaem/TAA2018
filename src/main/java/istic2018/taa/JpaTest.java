30e095e8bcd5c165b3889b20c49b56e3d1c0863e
JodAetaem <clarzi@gmail.com>
2018-09-17 18:15:28 +0200

    test du DAO Lieu

--- src/main/java/istic2018/taa/JpaTest.java
+++ src/main/java/istic2018/taa/JpaTest.java
@@ -5,12 +5,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import entity.Lieu;
+import entity.LieuId;
 import entity.Region;
 import entity.Sport;
+import repository.ILieuDAO;
 import repository.IRegionDAO;
 import entity.Sportexterieur;
 import entity.Sportinterieur;
 import repository.ISportDAO;
+import repository.LieuDAO;
 import repository.RegionDAO;
 import repository.SportDAO;
 
@@ -47,13 +51,22 @@ public class JpaTest {
 		sporte.setLabel("sportexterieur");
 		ISportDAO<Sport,Long> sportDAO2 = new SportDAO(manager,Sport.class);
 		sportDAO2. create(sporte);
-		manager.close();
 		
 		Region region = new Region();
 		region.setCode(123);
 		region.setName("Maregion");
 		IRegionDAO<Region, Long> regionDao = new RegionDAO(manager, Region.class);
 		regionDao.create(region);
+		
+		Lieu lieu = new Lieu();
+		LieuId lid = new LieuId();
+		lid.setCodePostal("35510");
+		lid.setLabelVille("Cessong");
+		lieu.setId(lid);
+		ILieuDAO<Lieu, LieuId> lieuDAO = new LieuDAO<Lieu, LieuId>(manager, Lieu.class);
+		lieuDAO.create(lieu);
+		manager.close();
+
 	}
 
 }
