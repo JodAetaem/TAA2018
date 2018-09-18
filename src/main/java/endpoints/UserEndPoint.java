343de85f39f61c1cfb2e6ca5b06bb17c6fa6e1f1
redeyes361 <redeyes_361@hotmail.com>
2018-09-18 11:54:13 +0200

    add post

--- src/main/java/endpoints/UserEndPoint.java
+++ src/main/java/endpoints/UserEndPoint.java
@@ -4,28 +4,35 @@ import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import istic2018.taa.EntityManagerHelper;
 import repository.IUserDAO;
 import repository.UserDAO;
 
 @Path("/user")
+@Api(value="User resource")
 public class UserEndPoint {
 
 	private static final Logger logger = Logger.getLogger(UserEndPoint.class.getName());
 	
 	EntityManager manager = EntityManagerHelper.getEntityManager();
-	EntityTransaction tx = manager.getTransaction();
 
+	
 	@GET
-	@Path("{id}")
+	@Path("/id/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value="Find user by id")
 	public User getUserById(@PathParam("id") long id) {
 		User u = new User();
 		IUserDAO<User,Long> userDAO = new UserDAO(manager,User.class);
@@ -35,9 +42,9 @@ public class UserEndPoint {
 
 		return u;
 	}
-/*
+
 	@GET
-	@Path("{pseudo}")
+	@Path("/pseudo/{pseudo}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public User getUserByPseudo(@PathParam("pseudo") String pseudo) {
 		User u = new User();
@@ -46,6 +53,16 @@ public class UserEndPoint {
 		u = userDAO.findByPseudo(pseudo);
 		logger.warning("coucou!!\n\n");
 		return u;
-	}*/
+	}
+	
+	@POST
+	@Consumes("application/json")
+	public Response addUser(User user){
+		
+		IUserDAO<User, Long> userDAO = new UserDAO(manager, User.class);
+		userDAO.create(user);
+		
+		return Response.noContent().build();
+	}
 
 }
